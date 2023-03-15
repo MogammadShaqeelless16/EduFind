5b7aef03be39fa0754fa829168658f8941f63ba1
MogammadShaqeelless16 <shaqeelless4@gmail.com>
2023-03-15 13:35:55 +0200

    update to login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -63,7 +63,7 @@ function App() {
 
       //save user ot mongo db..
       if (profileObj){
-        const response = await fetch('http://localhost:8080/api/v1/users',{
+        const response = await fetch('https://edufind.onrender.com/api/v1/users',{
           method: 'POST',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -138,7 +138,7 @@ function App() {
         <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
         <RefineSnackbarProvider>
           <Refine
-            dataProvider={dataProvider("http://localhost:8080/api/v1")}
+            dataProvider={dataProvider("https://edufind.onrender.com/api/v1")}
             notificationProvider={notificationProvider}
             ReadyPage={ReadyPage}
             catchAll={<ErrorComponent />}
